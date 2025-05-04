227c7b3f1168d35d7a0de491a89ed5ecdd42e350
AnshMNSoni <anshsoni702@gmail.com>
2025-05-04 21:17:18 +0530

    added live score feature

--- obstacles.py
+++ obstacles.py
@@ -1,29 +1,27 @@
 import turtle as t
-from turtle import Turtle, Screen
+from turtle import Turtle
 import random
 
-screen = Screen()
 class Obstacles(Turtle):
     def __init__(self):
         super().__init__()
         self.hideturtle()
         self.car_list = []
-        
+
     def random_clr(self):
         t.colormode(255)
         r = random.randint(0, 255)
         g = random.randint(0, 255)
         b = random.randint(0, 255)
-        tpl = (r, g, b)
-        return tpl
-    
-    def create_cars(self):
-        screen.tracer(0)
-        new_turtle = Turtle()
-        new_turtle.penup()
-        new_turtle.shape('square')
-        y = random.randint(-265, 265)
-        new_turtle.goto(500, y)
-        new_turtle.shapesize(1, 3, 1)
-        new_turtle.color(self.random_clr())
-        self.car_list.append(new_turtle)
+        return (r, g, b)
+
+    def create_cars(self, level):
+        if random.randint(1, 6) <= 2 + min(level, 3):  # more cars as level increases
+            new_turtle = Turtle()
+            new_turtle.penup()
+            new_turtle.shape('square')
+            new_turtle.shapesize(1, 3)
+            new_turtle.color(self.random_clr())
+            y = random.randint(-350, 350)
+            new_turtle.goto(500, y)
+            self.car_list.append(new_turtle)
